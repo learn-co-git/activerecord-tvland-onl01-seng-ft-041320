e4624c1ef4eeb4c2cbf1fd6d86349eb404c69afc
Neil Empey <argus.two.2@gmail.com>
2020-05-17 21:04:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 10) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -6,23 +6,23 @@ ActiveRecord::Schema.define(version: 10) do
   end
 
   create_table "characters", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
     t.integer "show_id"
-    t.string "catchphrase"
     t.integer "actor_id"
+    t.string  "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
-    t.string "call_letters"
+    t.string  "call_letters"
     t.integer "channel"
   end
 
   create_table "shows", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
     t.integer "network_id"
-    t.string "genre"
-    t.string "day"
-    t.string "season"
+    t.string  "day"
+    t.string  "season"
+    t.string  "genre"
   end
 
 end
